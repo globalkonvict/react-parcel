165a96039411ff4e36c3ac605e96e7305ca90d10
Sarthak Dwivedi <sarthakwarlock@gmail.com>
2020-04-12 01:02:55 +0530

    Fixed few mistakes

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,6 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:import/recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:import/recommended',
-    'plugin:prettier/recommended',
   ],
   plugins: ['react-hooks'],
   globals: {
